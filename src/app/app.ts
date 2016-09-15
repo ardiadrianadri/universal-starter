359e273f9d829a4a7783ff9c09012356ab9fba1b
Adrian Ferreres <ardiadrianadri@gmail.com>
2016-09-15 10:22:59 +0200

    Starting the POC addaption

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: 'Hello Universal App'
+  template: '<h1>Hello Universal App</h1>'
 })
 export class App {
 
